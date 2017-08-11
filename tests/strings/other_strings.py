1956c66414a5d7e92a12fdafc2c0660d31eec4c4
NAITOH Jun <naitoh@gmail.com>
2017-08-11 13:33:37 +0900

    Fixed test case. (Name) * tests/strings/strings_in_strings.py

---
+++ tests/strings/other_strings.py
@@ -0,0 +1,3 @@
+# Test other strings:
+print("\\u0000")
+print(b"\\x00")
--- tests/strings/strings_in_strings.py
+++ tests/strings/strings_in_strings.py
@@ -1,5 +1,1 @@
 print(""""Hello World!" <- should 'mess' up '''thing'''?""")
-
-# Test other strings:
-print(b"\\x00")
-print("\\u0000")
