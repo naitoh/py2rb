219f029b2711acf5e7873a57fbf3874ccc732543
NAITOH Jun <naitoh@gmail.com>
2017-10-07 18:29:39 +0900

    Added test case.(numpy) * tests/numpy/type.py

---
+++ tests/numpy/type.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+import numpy as np
+
+print(np.int8)
+print(np.int16)
+print(np.int32)
+print(np.int64)
+print(np.int)
+print(np.uint8)
+print(np.uint16)
+print(np.uint32)
+print(np.uint64)
+print(np.uint)
+print(np.float32)
+print(np.float64)
