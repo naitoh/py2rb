39d212a07a3c5ee425ac5ad5f014e903b28f654f
NAITOH Jun <naitoh@gmail.com>
2017-10-09 17:58:35 +0900

    Added test case.(numpy) * tests/numpy/asarray.py

---
+++ tests/numpy/asarray.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def print_matrix(data):
+    data_i = []
+    for i in list(data):
+        data_j = []
+        for j in i:
+            data_j.append(int("%d" % j))
+        data_i.append(data_j)
+    print(data_i)
+
+x = np.asarray([[1.,2.],[3.,4.]])
+print_matrix(x)
