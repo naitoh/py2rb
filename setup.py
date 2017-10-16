1ef94030514f5863997e1b2a2f0dae0a3c3daae1
Eli Sadoff <snood1205@gmail.com>
2017-10-16 17:50:53 -0400

    Create package to allow for pypi upload Closes #1

---
+++ setup.py
@@ -0,0 +1,44 @@
+"""
+A code translator using AST from Python to Ruby.
+This is basically a NodeVisitor with ruby output.
+See:
+https://docs.python.org/3/library/ast.html
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='py2rb',
+    version='0.1.0',
+    description='A code translator using AST from Python to Ruby',
+    long_description=long_description,
+    url='https://github.com/naitoh/py2rb',
+    author='NAITOH Jun',
+    author_email='naitoh@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
+    keywords='python ruby',
+    packages=find_packages(exclude=['tests']),
+    install_requires=[
+        'pyyaml',
+    ],
+    entry_points={
+        'console_scripts': [
+            'py2rb=py2rb:main'
+        ]
+    }
+)
