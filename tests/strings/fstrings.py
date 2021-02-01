2441ac83322056c334a27ea3383d74830f3f6d82
Ben Bowers <nanobowers@gmail.com>
2021-01-31 23:01:20 -0500

    initial fstring test

---
+++ tests/strings/fstrings.py
@@ -0,0 +1,17 @@
+a = "dog"
+b = 'cat'
+
+print(f"{a}{b}{a}{b}")
+print(f'{a}{b}{a}{b}')
+print(f"""{a}{b}{a}{b}""")
+
+c = 1.234E-6
+d = -177
+
+print(f"float: {c} int: {d}")
+
+e = (1, 3, 5)
+f = ["b", 'c', 'd', 7]
+
+print(f'tuple: {e} list: {f}')
+
